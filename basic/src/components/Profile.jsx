e2de62e9bb01a97d1ca5e6661705bf912c6cd40f
imminpie <mjkang0715@gmail.com>
2024-02-09 14:30:44 +0900

    feat: Props 사용해보기

--- basic/src/components/Profile.jsx
+++ basic/src/components/Profile.jsx
@@ -1,12 +1,11 @@
-export default function Profile() {
+export default function Profile(props) {
+  const { image, name, title } = props;
+
   return (
     <div className='profile'>
-      <img
-        className='photo'
-        src='https://images.unsplash.com/photo-1527980965255-d3b416303d12?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1180&q=80'
-        alt='avatar'
-      />
-      <h1>James Kim</h1>
+      <img className='photo' src={image} alt='avatar' />
+      <h1>{name}</h1>
+      <p>{title}</p>
     </div>
   );
 }
