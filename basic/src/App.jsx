dcc819128c7b66cb53c22edae12e36cc020f7b05
imminpie <mjkang0715@gmail.com>
2024-02-09 14:13:55 +0900

    feat: JSX 문법

--- basic/src/App.jsx
+++ basic/src/App.jsx
@@ -1,7 +1,16 @@
 import './App.css';
 
 function App() {
-  return <></>;
+  const list = ['우유', '딸기', '바나나'];
+
+  return (
+    <>
+      <h1 style={{ color: 'orange' }}>Hello!</h1>
+      {list.map((item) => (
+        <li>{item}</li>
+      ))}
+    </>
+  );
 }
 
 export default App;
