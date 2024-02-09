71cedb3dd4258a3ebba65e729953fdb72418ca49
imminpie <mjkang0715@gmail.com>
2024-02-09 14:35:27 +0900

    feat: 새로운 개발자 태그 만들기

--- basic/src/components/Profile.jsx
+++ basic/src/components/Profile.jsx
@@ -1,9 +1,10 @@
 export default function Profile(props) {
-  const { image, name, title } = props;
+  const { image, name, title, isNew } = props;
 
   return (
     <div className='profile'>
       <img className='photo' src={image} alt='avatar' />
+      {isNew && <span className='new'>New</span>}
       <h1>{name}</h1>
       <p>{title}</p>
     </div>
