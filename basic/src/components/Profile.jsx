f41db4c87d91df49d068d79719302704289a470c
imminpie <mjkang0715@gmail.com>
2024-02-09 14:39:14 +0900

    feat: 아바타 컴포넌트 만들기

--- basic/src/components/Profile.jsx
+++ basic/src/components/Profile.jsx
@@ -1,10 +1,11 @@
+import Avatar from './Avatar';
+
 export default function Profile(props) {
   const { image, name, title, isNew } = props;
 
   return (
     <div className='profile'>
-      <img className='photo' src={image} alt='avatar' />
-      {isNew && <span className='new'>New</span>}
+      <Avatar image={image} isNew={isNew} />
       <h1>{name}</h1>
       <p>{title}</p>
     </div>
