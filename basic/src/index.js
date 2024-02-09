c85e4790aa53eeec60cf58c27ea63d934a02d35a
imminpie <mjkang0715@gmail.com>
2024-02-09 23:50:30 +0900

    feat: Form 을 만드는 법

--- basic/src/index.js
+++ basic/src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import AppMentors from './AppMentors';
+import AppForm from './AppForm';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <AppMentors />
+    <AppForm />
   </React.StrictMode>
 );
