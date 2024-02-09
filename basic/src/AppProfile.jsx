c158547410e38e03663084eef57b225a3ccfe0aa
imminpie <mjkang0715@gmail.com>
2024-02-09 14:26:41 +0900

    feat: Profile 첫 컴포넌트 만들기

---
+++ basic/src/AppProfile.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import Profile from './components/Profile';
+
+export default function AppProfile() {
+  return (
+    <>
+      <Profile />
+      <Profile />
+      <Profile />
+    </>
+  );
+}
