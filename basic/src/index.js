1dd4482f7f54c05febc0963e501f2d208b6b569a
imminpie <mjkang0715@gmail.com>
2024-02-10 00:03:00 +0900

    feat: 컴포넌트의 재사용2 🍯 (카드)

--- basic/src/index.js
+++ basic/src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import AppWrap from './AppWrap';
+import AppCard from './AppCard';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <AppWrap />
+    <AppCard />
   </React.StrictMode>
 );
