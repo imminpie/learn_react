354a08032ae7bcbe0ee4ef9806d55ecbb4da698a
imminpie <mjkang0715@gmail.com>
2024-02-16 13:55:59 +0900

    feat: 로딩, 에러 상태 추가

--- basic/src/index.js
+++ basic/src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import AppTheme from './AppTheme';
+import AppProducts from './basic/AppProducts';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <AppTheme />
+    <AppProducts />
   </React.StrictMode>
 );
